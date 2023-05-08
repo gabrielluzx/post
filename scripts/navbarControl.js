3703945c3ad03e100975cbfff56be34031c6ab51
gabriel luz <luz.gabriel010@gmail.com>
2023-05-08 10:56:50 -0300

    Melhorando a responsividade e reestilizando o menu do footer

--- scripts/navbarControl.js
+++ scripts/navbarControl.js
@@ -1,12 +1,14 @@
 var navbarIsDisplayed = false;
-document.getElementById("menu-icon").addEventListener("click", controlNavbar);
-document.getElementById("close-icon").addEventListener("click", controlNavbar);
+document.querySelector(".menu-icon").addEventListener("click", controlNavbar);
+document.querySelector(".close-icon").addEventListener("click", controlNavbar);
 function controlNavbar(){
-    var navbar = document.getElementById("navbar");
+    var navbar = document.getElementsByClassName("navmenu")[0];
     if(navbarIsDisplayed){
+        document.querySelector(".menu-icon").style.display = "block";
         navbar.style.display = "none";
         navbarIsDisplayed = false;
     }else{
+        document.querySelector(".menu-icon").style.display = "none";
         navbar.style.display = "block";
         navbarIsDisplayed = true;
     }
